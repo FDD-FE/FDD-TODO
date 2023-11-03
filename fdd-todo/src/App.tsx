6eb7765eda599d6dab219524cad360baff431d73
kwakseongjae <gkffhdnls13@gmail.com>
2023-11-04 03:10:40 +0900

    CHORE: CRA및 기본 세팅 - components, hooks, styles 폴더 생성 - title 및 favicon 변경 - react-router-dom 사용이 불필요하다고 판단하여 제거

---
+++ fdd-todo/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import ToDoList from "./components/ToDoList";
+import GlobalStyle from "./styles/GlobalStyle";
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <ToDoList />
+    </>
+  );
+}
+
+export default App;
---
+++ fdd-todo/src/components/ToDoList.tsx
@@ -0,0 +1,5 @@
+const ToDoList = () => {
+  return <div>ToDoList 입니다 !</div>;
+};
+
+export default ToDoList;
